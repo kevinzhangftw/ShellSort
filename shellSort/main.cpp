2cf1eccbb7e171cbf1333bbeee6f194c3b43686d
Kevin Zhang <kevinctzhang@gmail.com>
2016-02-15 21:31:20 -0800

    functional

--- shellSort/main.cpp
+++ shellSort/main.cpp
@@ -17,11 +17,28 @@ int main(int argc, const char * argv[]) {
     // insert code here...
     int arr[5] = {3,1,2,5,4};
     int arr_size = 5;
-    printArray(arr, arr_size);
     shellSort(arr, arr_size);
     printArray(arr, arr_size);
-    cout << "Hello, World!\n";
     return 0;
+}
+
+int shellSort(int arr[], int arr_size){
+    int barOps=0;
+    int gap = 0;
+    int i = 0;
+    int j = 0;
+    int temp = 0;
+    
+    for (gap = arr_size/2; gap>0; gap/=2) {
+        for (i=gap; i<arr_size; ++i) {
+            for (j=i-gap; j>=0 && arr[j]>arr[j+gap]; j-=gap) {
+                temp = arr[j];
+                arr[j] = arr[j+gap];
+                arr[j+gap] = temp;
+            }
+        }
+    }
+    return barOps;
 }
 
 void printArray(int arr[], int arr_size){
@@ -30,10 +47,4 @@ void printArray(int arr[], int arr_size){
         cout<< " " <<arr[i];
     }
     //print out that element
-}
-
-int shellSort(int arr[], int arr_size){
-    int barOps=0;
-    //
-    return barOps;
 }
